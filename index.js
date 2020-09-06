e10d7bdaccbcc7582b50115b53ab5c6ea38925f7
ishank622 <ishank.agarwal@algoscale.com>
2020-09-06 17:25:33 +0530

    clustering before PM2

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+process.env.UV_THREADPOOL_SIZE = 1; //so that every child has only 1 thread
+
 const cluster = require("cluster");
+const crypto = require("crypto");
 
 if (cluster.isMaster) {
-  //to create children
+  //to create children (keep them equal to no. of physical/logical cores)
   cluster.fork();
-  //   cluster.fork();
-  //   cluster.fork();
-  //   cluster.fork();
+  cluster.fork();
 } else {
   // Child
   const express = require("express");
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
+  // function doWork(duration) {
+  //   const start = Date.now();
+  //   while (Date.now() - start < duration) {}
+  // }
 
   app.get("/", (req, res) => {
-    doWork(5000);
+    // doWork(5000);
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {});
     res.send("Hi there!");
   });
 
@@ -25,5 +27,5 @@ if (cluster.isMaster) {
     res.send("Hi there fast!");
   });
 
-  app.listen(3000);
+  app.listen(3001);
 }
