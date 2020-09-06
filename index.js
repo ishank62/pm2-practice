a9ca5301ad8fc5ac46cb8cf9e1436db3cf2b7e01
ishank622 <ishank.agarwal@algoscale.com>
2020-09-06 18:11:03 +0530

    pm2 finished

--- index.js
+++ index.js
@@ -3,29 +3,17 @@ process.env.UV_THREADPOOL_SIZE = 1; //so that every child has only 1 thread
 const cluster = require("cluster");
 const crypto = require("crypto");
 
-if (cluster.isMaster) {
-  //to create children (keep them equal to no. of physical/logical cores)
-  cluster.fork();
-  cluster.fork();
-} else {
-  // Child
-  const express = require("express");
-  const app = express();
+const express = require("express");
+const app = express();
 
-  // function doWork(duration) {
-  //   const start = Date.now();
-  //   while (Date.now() - start < duration) {}
-  // }
+app.get("/", (req, res) => {
+  // doWork(5000);
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {});
+  res.send("Hi there!");
+});
 
-  app.get("/", (req, res) => {
-    // doWork(5000);
-    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {});
-    res.send("Hi there!");
-  });
+app.get("/fast", (req, res) => {
+  res.send("Hi there fast!");
+});
 
-  app.get("/fast", (req, res) => {
-    res.send("Hi there fast!");
-  });
-
-  app.listen(3001);
-}
+app.listen(3001);
