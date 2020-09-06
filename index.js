9c61fef2d3461ab08eabc2a5f10e6a72da86993a
ishank622 <ishank.agarwal@algoscale.com>
2020-09-06 18:31:44 +0530

    worker-threads iplemented

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-process.env.UV_THREADPOOL_SIZE = 1; //so that every child has only 1 thread
+// process.env.UV_THREADPOOL_SIZE = 1; //so that every child has only 1 thread
 
-const cluster = require("cluster");
+// const cluster = require("cluster");
 const crypto = require("crypto");
 
 const express = require("express");
 const app = express();
 
+const Worker = require("webworker-threads").Worker;
+
 app.get("/", (req, res) => {
   // doWork(5000);
-  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {});
-  res.send("Hi there!");
+  // crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {});
+  // res.send("Hi there!");
+  const worker = new Worker(function () {
+    this.onmessage = function () {
+      let counter = 0;
+      while (counter < 1e9) {
+        counter++;
+      }
+    };
+    postMessage(counter);
+  });
+
+  worker.onmessage = function (message) {
+    console.log(message.data);
+    res.send("" + message.data);
+  };
+
+  worker.postMessage();
 });
 
 app.get("/fast", (req, res) => {
